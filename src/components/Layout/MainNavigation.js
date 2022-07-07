8f982c7dc387faae0f0bbf14e07e5d98c4065019
Dhruv Rana <dhruvrana0000@gmail.com>
2022-07-07 19:11:16 +0530

    Enabling the Logout Button

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
 
@@ -6,8 +7,14 @@ import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
-
+  const history = useHistory();
   const isLoggedIn = authCtx.isLoggedIn;
+  const logoutHandler = () => {
+    authCtx.logout();
+
+    //Option // Redirect
+    // history.replace("/auth");
+  };
   return (
     <header className={classes.header}>
       <Link to="/">
@@ -27,7 +34,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
